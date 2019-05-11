03cf2b72ba5d3a5733494c893266d3679d42827c
Oneted11 <muriithited@gmail.com>
2019-05-11 20:11:15 +0300

    tcp-chat:notifying that some one joined the chat

--- tcp-chat/index.js
+++ tcp-chat/index.js
@@ -1,10 +1,12 @@
 const net = require("net");
 
-let count = 0;
+let count = 0,
+  users = {};
 //create server
 const server = net.createServer(conn => {
   // handle connection
-//   console.log("\033[90mnew connection!\033[39m");
+  //   console.log("\033[90mnew connection!\033[39m");
+  let nickname;
   conn.write(
     "\n > welcome to \033[92mNode-chat\033[39m!" +
       "\n > " +
@@ -12,10 +14,25 @@ const server = net.createServer(conn => {
       " other people connected at this time." +
       "\n > please write your name and press enter: "
   );
-  count++;
+
   conn.on("data", data => {
-    console.log("data");
+    // data = data.replace("\r\n", "");
+    if (!nickname) {
+      if (users[data]) {
+        conn.write("\033[93m> nickname already in use.try again: \033[39m ");
+        return;
+      } else {
+        nickname = data;
+        users[nickname] = conn;
+        for (let i in users) {
+          users[i].write(
+            "\033[90m > " + nickname + " joined the room\033[39m\n"
+          );
+        }
+      }
+    }
   });
+  count++;
   conn.on("close", () => {
     count--;
   });
