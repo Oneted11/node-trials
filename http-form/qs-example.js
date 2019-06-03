fe80a7c16f65a59684b5632727d1df7af5f72732
Oneted11 <muriithited@gmail.com>
2019-06-03 13:23:46 +0300

    made form parse body with querystring

---
+++ http-form/qs-example.js
@@ -0,0 +1,2 @@
+console.log(require("querystring").parse("name= tedeos"))
+console.log(require("querystring").parse("q=tedeaos+ranch"))
---
+++ http-form/server.js
@@ -0,0 +1,31 @@
+const qs = require("querystring");
+require("http")
+  .createServer((req, res) => {
+    if (req.url == "/") {
+      res.writeHead(200, { "content-Type": "text/html" });
+      res.end(
+        [
+          "<form method='POST' action='url'>",
+          "<h1>My form</h1>",
+          "<fieldset>",
+          "<label>Pesonal Information</label>",
+          "<p>what is your name</p>",
+          '<input type="text" name="name"></input>',
+          "<p><button>Submit</button></p>",
+          "</form>"
+        ].join("")
+      );
+    } else if ("/url" == req.url && "POST" == req.method) {
+      let body = "";
+      req.on("data", function(chunk) {
+        body += chunk;
+      });
+      req.on("end", function() {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(
+            "<p> Your name is <em><b>" + qs.parse(body).name + "</b></em></p>"
+          );
+        });
+    }
+  })
+  .listen(3000);
